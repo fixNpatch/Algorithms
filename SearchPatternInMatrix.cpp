479075cb25a5bbd32eda9fcc2570533b42f28f35
Anton <titov.ant.workmail@gmail.com>
2017-01-07 00:45:32 +0400

    Tests:
    --#1
    4
    a a a a
    a a a a
    a a a a
    a a a a
    2
    a ?
    
    --#2
    3
    a b c
    a b a
    b a b
    2
    a b

--- SearchPatternInMatrix.cpp
+++ SearchPatternInMatrix.cpp
@@ -6,15 +6,32 @@
 using namespace std;
 
 int check(string &t, string &p, int x) {
-	int summ;
-	return summ;
-}
+	int sum = 0, i = 0, j, end = p.length();
+	bool brk;
+	while (i < x) {
+		if (i + end > x) break;
+		else {
+			brk = false;
+			j = 0;
+			while (j < end) {
+				if ((t[i + j] != p[j]) && (p[j] != '?')) {
+					brk = true;
+				}
+				j++;
+			}
+			if (!brk) sum++;
+		}
+		i++;
+	}
+	return sum;
+};
 
 int main() {
-	int n;
+	int n, m;
 	cin >> n;
 	vector<string> hV(n);
 	vector<string> vV(n);
+	
 	char cure;
 	for (int i = 0; i < n; i++)	{
 		for (int j = 0; j < n; j++) {
@@ -28,7 +45,14 @@ int main() {
 			vV[i].push_back(hV[j][i]);
 		}
 	}
-	cout << "\nhV\n";
+	cin >> m;
+	string pattern;
+	for (int i = 0; i < m; i++) {
+		cin >> cure;
+		pattern += cure;
+	}
+
+	/*cout << "\nhV\n";
 	for (int i = 0; i < n; i++)	{
 		for (int j = 0; j < n; j++)
 			cout << hV[i][j] << " ";
@@ -40,7 +64,17 @@ int main() {
 			cout << vV[i][j] << " ";
 		cout << endl;
 	}
-
 	
+	cout << "\npattern\n";
+	for (int i = 0; i < n; i++)
+		cout << pattern[i];*/
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += check(hV[i], pattern, n);
+		ans += check(vV[i], pattern, n);
+	}
+	cout << "Search Completed\n";
+	cout << "Answer is " << ans << endl;
 	return 0;
 }
